a1fc54405a4ccd07b21cfa63ec08a3ae1d3a7721
yeana-dev <yeana.dev@gmail.com>
2021-08-25 10:58:20 -0500

    🍣 Comfort Level displays as stars instead of a number

--- client/src/components/Notes.jsx
+++ client/src/components/Notes.jsx
@@ -1,11 +1,13 @@
 export default function Notes(props) {
+  const level = parseInt(props.note.fields.comfortLevel);
+  const comfortLevel = "⭐️".repeat(level);
   return (
     <div className="note-card">
       <h1>{props.note.fields.title}</h1>
       <small>
         {props.note.createdTime} / {props.note.fields.category}
       </small>
-      <h3>Comfort Level: {props.note.fields.comfortLevel}/5</h3>
+      <h3>{comfortLevel}</h3>
       <article>{props.note.fields.content}</article>
     </div>
   );
