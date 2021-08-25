5a64c344f0508e18eb3027499aa0296f5981d5ee
yeana-dev <yeana.dev@gmail.com>
2021-08-25 10:11:46 -0500

    🌽 Integrated EmailJS to contact form

--- client/src/components/ContactForm.jsx
+++ client/src/components/ContactForm.jsx
@@ -1,3 +1,34 @@
+import emailjs from "emailjs-com";
+
 export default function ContactForm() {
-  return <h1>I am Contact me Form!</h1>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_iiwc60s",
+        "template_2tmy79s",
+        e.target,
+        "user_YOH18ApgTgggDNs7eGZD9"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+  return (
+    <form id="contact-form" onSubmit={handleSubmit}>
+      <input type="hidden" name="contact_number" />
+      <label htmlFor="name">Name</label>
+      <input type="text" name="name" />
+      <label htmlFor="email">E-mail</label>
+      <input type="email" name="email" />
+      <label htmlFor="message">Message</label>
+      <textarea name="message" />
+      <input type="submit" />
+    </form>
+  );
 }
