ca6b5a90da1daf0eb9a88dae5051c5adc8b91bd2
yeana-dev <yeana.dev@gmail.com>
2021-08-26 19:06:56 -0500

    🍘 Styled and added alert to Contact me form

--- client/src/components/ContactForm.jsx
+++ client/src/components/ContactForm.jsx
@@ -1,6 +1,15 @@
 import emailjs from "emailjs-com";
+import Form from "react-bootstrap/Form";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Alert from "react-bootstrap/Alert";
+
+import { useState } from "react";
+
+import "./style/ContactForm.css";
 
 export default function ContactForm() {
+  const [show, setShow] = useState(false);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     emailjs
@@ -12,23 +21,49 @@ export default function ContactForm() {
       )
       .then(
         (result) => {
-          console.log(result.text);
+          console.log(result);
         },
         (error) => {
           console.log(error.text);
         }
       );
+    setShow(true);
   };
   return (
     <form id="contact-form" onSubmit={handleSubmit}>
+      <div id="contact-greeting">
+        <div id="contact-greeting-left">
+          <h3>Hello! 안녕하세요! Thank you for visiting! 👋</h3> My name is
+          Yeana 🇰🇷 , and this is my second project from General Assembly SEI.
+          <br />
+          Please feel free to send me any feedback or comment here!
+        </div>
+      </div>
       <input type="hidden" name="contact_number" />
-      <label htmlFor="name">Name</label>
-      <input type="text" name="from_name" />
-      <label htmlFor="email">E-mail</label>
-      <input type="email" name="reply_to" />
-      <label htmlFor="message">Message</label>
-      <textarea name="message" />
-      <input type="submit" />
+      <div id="contact-top">
+        <FloatingLabel id="contact-name" label="Name">
+          <Form.Control type="text" placeholder="Name" name="from_name" />
+        </FloatingLabel>
+        <FloatingLabel id="contact-email" label="Email">
+          <Form.Control type="email" placeholder="Email" name="reply_to" />
+        </FloatingLabel>
+      </div>
+      <div className="contact-message-container">
+        <FloatingLabel label="Message">
+          <Form.Control
+            id="contact-message"
+            placeholder="Message"
+            as="textarea"
+            name="message"
+          />
+        </FloatingLabel>
+      </div>
+      <div id="contact-bottom">
+        <input type="submit" id="contact-submit" />
+        <Alert variant="success" id="contact-alert" show={show}>
+          Message Sent!
+        </Alert>
+      </div>
     </form>
   );
 }
