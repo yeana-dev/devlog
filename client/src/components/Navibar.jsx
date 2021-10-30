d3b6b0723315906c97ccee86b9115c47fe791836
yeana-dev <yeana.dev@gmail.com>
2021-10-30 16:18:40 -0500

    Fixed bug: navbar not collapsing after item is clicked

--- client/src/components/Navibar.jsx
+++ client/src/components/Navibar.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import "bootstrap/dist/css/bootstrap.css";
@@ -6,32 +6,57 @@ import "./style/Navibar.css";
 
 export default function Navibar(props) {
   return (
-    <Navbar expand="lg" fixed="top" id="nav-container">
-      <NavLink id="nav-logo" to="/">{`<Devlog />`}</NavLink>
+    <Navbar collapseOnSelect expand="lg" fixed="top" id="nav-container">
+      <Nav.Link
+        id="nav-logo"
+        to="/"
+        eventKey="home"
+        as={Link}
+      >{`<Devlog />`}</Nav.Link>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav id="nav-menu-items">
-          <NavLink to="/new" className="nav-item">
+          <Nav.Link to="/new" eventKey="new" as={Link} className="nav-item">
             <i class="material-icons">note_add</i> New Note
-          </NavLink>
-          <NavLink to="/" className="nav-item" exact>
+          </Nav.Link>
+          <Nav.Link to="/" eventKey="all" as={Link} className="nav-item" exact>
             <i class="material-icons">code</i> View all
-          </NavLink>
+          </Nav.Link>
           {/* List a nav item for each category user created */}
           {props.category.map((category) => (
-            <NavLink to={`/${category}`} className="sub-category">
+            <Nav.Link
+              to={`/${category}`}
+              className="sub-category"
+              eventKey={category}
+              as={Link}
+            >
               {category}
-            </NavLink>
+            </Nav.Link>
           ))}
-          <NavLink to="/project" className="nav-item">
+          <Nav.Link
+            to="/project"
+            eventKey="projects"
+            as={Link}
+            className="nav-item"
+          >
             <i class="material-icons">web</i> Projects
-          </NavLink>
-          <NavLink to="/new-project" className="sub-category">
+          </Nav.Link>
+          <Nav.Link
+            to="/new-project"
+            eventKey="new project"
+            as={Link}
+            className="sub-category"
+          >
             <li>New Project</li>
-          </NavLink>
-          <NavLink to="/contact" className="nav-item">
+          </Nav.Link>
+          <Nav.Link
+            to="/contact"
+            className="nav-item"
+            eventKey="contact"
+            as={Link}
+          >
             <i class="material-icons">mail</i> Contact
-          </NavLink>
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
